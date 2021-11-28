4c2344bd7e6f9e347fccf017ae9eb3a94b58b885
cariocaphil <cariocaphil@gmail.com>
2021-11-28 17:53:11 +0100

    format

--- trader.py
+++ trader.py
@@ -5,25 +5,28 @@ import time
 SEC_KEY = 'bcO995J1nB2W7iWbzIkXv4foX0GKaQJAYT8pX1fN'
 PUB_KEY = 'PKRQE96HW8BM6TO9V13V'
 BASE_URL = 'https://paper-api.alpaca.markets'
-api = tradeapi.REST(key_id= PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL)
+api = tradeapi.REST(key_id=PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL)
 
 symb = "SPY"
 
 while True:
     print("")
     print("Checking Price")
-    
-    market_data = api.get_barset(symb, 'minute', limit=5) # Get one bar object for each of the past 5 minutes
 
-    close_list = [] # This array will store all the closing prices from the last 5 minutes
+    # Get one bar object for each of the past 5 minutes
+    market_data = api.get_barset(symb, 'minute', limit=5)
+
+    close_list = []  # This array will store all the closing prices from the last 5 minutes
     for bar in market_data[symb]:
-        close_list.append(bar.c) # bar.c is the closing price of that bar's time interval
+        # bar.c is the closing price of that bar's time interval
+        close_list.append(bar.c)
 
-    close_list = np.array(close_list, dtype=np.float64) # Convert to numpy array
+    # Convert to numpy array
+    close_list = np.array(close_list, dtype=np.float64)
     ma = np.mean(close_list)
-    last_price = close_list[4] # Most recent closing price
+    last_price = close_list[4]  # Most recent closing price
 
     print("Moving Average: " + str(ma))
     print("Last Price: " + str(last_price))
-    
-    time.sleep(60) # Wait one minute before retreiving more market data
+
+    time.sleep(60)  # Wait one minute before retreiving more market data
