f25318177772c73dcdf679d5ac81d1b70b9303c0
cariocaphil <cariocaphil@gmail.com>
2021-11-28 18:37:18 +0100

    add backtesting

--- trader.py
+++ trader.py
@@ -2,54 +2,66 @@ import alpaca_trade_api as tradeapi
 import numpy as np
 import time
 
-SEC_KEY = 'qGvrvSjjSfeS6QBsBW90VfFs5gHnFSZwylAaQUsw'
-PUB_KEY = 'PK7096O42LK4VQCUZHYW'
+SEC_KEY = ''
+PUB_KEY = ''
 BASE_URL = 'https://paper-api.alpaca.markets'
 api = tradeapi.REST(key_id=PUB_KEY, secret_key=SEC_KEY, base_url=BASE_URL)
 
 symb = "SPY"
 pos_held = False
+hours_to_test = 2
 
-while True:
-    print("")
-    print("Checking Price")
+print("Checking Price")
+# Pull market data from the past 60x minutes
+market_data = api.get_barset(symb, 'minute', limit=(60 * hours_to_test))
 
-    # Get one bar object for each of the past 5 minutes
-    market_data = api.get_barset(symb, 'minute', limit=5)
+close_list = []
+for bar in market_data[symb]:
+    close_list.append(bar.c)
 
-    close_list = []  # This array will store all the closing prices from the last 5 minutes
-    for bar in market_data[symb]:
-        # bar.c is the closing price of that bar's time interval
-        close_list.append(bar.c)
 
-    # Convert to numpy array
-    close_list = np.array(close_list, dtype=np.float64)
-    ma = np.mean(close_list)
-    last_price = close_list[4]  # Most recent closing price
+print("Open: " + str(close_list[0]))
+print("Close: " + str(close_list[60 * hours_to_test - 1]))
+
+
+close_list = np.array(close_list, dtype=np.float64)
+startBal = 2000  # Start out with 2000 dollars
+balance = startBal
+buys = 0
+sells = 0
+
+
+for i in range(4, 60 * hours_to_test):  # Start four minutes in, so that MA can be calculated
+    ma = np.mean(close_list[i-4:i+1])
+    last_price = close_list[i]
 
     print("Moving Average: " + str(ma))
     print("Last Price: " + str(last_price))
 
-    # If MA is more than 10cents under price, and we haven't already bought
     if ma + 0.1 < last_price and not pos_held:
         print("Buy")
-        api.submit_order(
-            symbol=symb,
-            qty=1,
-            side='buy',
-            type='market',
-            time_in_force='gtc'
-        )
+        balance -= last_price
         pos_held = True
-    elif ma - 0.1 > last_price and pos_held:  # If MA is more than 10cents above price, and we already bought
+        buys += 1
+    elif ma - 0.1 > last_price and pos_held:
         print("Sell")
-        api.submit_order(
-            symbol=symb,
-            qty=1,
-            side='sell',
-            type='market',
-            time_in_force='gtc'
-        )  
+        balance += last_price
         pos_held = False
+        sells += 1
+    print(balance)
+    time.sleep(0.01)
 
-    time.sleep(60)  # Wait one minute before retreiving more market data
+print("")
+print("Buys: " + str(buys))
+print("Sells: " + str(sells))
+
+if buys > sells:
+    # Add back your equity to your balance
+    balance += close_list[60 * hours_to_test - 1]
+
+
+print("Final Balance: " + str(balance))
+
+print("Profit if held: " +
+      str(close_list[60 * hours_to_test - 1] - close_list[0]))
+print("Profit from algorithm: " + str(balance - startBal))
